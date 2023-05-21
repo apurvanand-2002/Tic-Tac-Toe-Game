9aed3b7bbca4e3b4d77b470550b2308cfef1bd98
Apurv Anand <apurvanand.2002@gmail.com>
2023-05-21 13:46:44 +0530

    Introduced players & toggled 'X' & 'O' continuously.

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -3,19 +3,24 @@ import Square from './Square'
 
 const Board = () => {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    const [isXNext, setIsXNext] = useState(false);
 
     console.log(squares);
 
     const handleSquareClick = (clickedPosition) => {
+        if (squares[clickedPosition]) {
+            return;
+        }
         setSquares((currentSquares) => {
             return currentSquares.map((squareValue, position) => {
                 if (clickedPosition === position) {
-                    return 'X';
+                    return isXNext ? 'X' : 'O';
                 }
 
                 return squareValue;
             });
         });
+        setIsXNext((currentIsXNext) => !currentIsXNext)
     };
     const renderSquare = (position) => {
         return (
