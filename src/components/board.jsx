c4830763bb9bc8ccdc1fb8689038d70c1b28f36f
Apurv Anand <apurvanand.2002@gmail.com>
2023-05-21 14:32:03 +0530

    Calculated winner & displayed status messages and winner statement.

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,27 +1,11 @@
-import { useState } from 'react'
 import Square from './Square'
 
-const Board = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [isXNext, setIsXNext] = useState(false);
+const Board = ({ squares, handleSquareClick }) => {
+
 
     console.log(squares);
 
-    const handleSquareClick = (clickedPosition) => {
-        if (squares[clickedPosition]) {
-            return;
-        }
-        setSquares((currentSquares) => {
-            return currentSquares.map((squareValue, position) => {
-                if (clickedPosition === position) {
-                    return isXNext ? 'X' : 'O';
-                }
 
-                return squareValue;
-            });
-        });
-        setIsXNext((currentIsXNext) => !currentIsXNext)
-    };
     const renderSquare = (position) => {
         return (
             <Square
